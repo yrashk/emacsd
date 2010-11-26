cdf25c373832bcdff50f042add4884098947a67c
Yurii Rashkovskii <yrashk@gmail.com>
2010-11-26 13:59:24 -0800

    Added coffee-mode

--- init.el
+++ init.el
@@ -12,4 +12,27 @@
 (add-to-list 'load-path "~/.emacs.d/color-theme")
 (require 'color-theme)
 (color-theme-initialize)
-(color-theme-robin-hood)
+(color-theme-arjen)
+
+;; Editor
+(setq-default tab-width 4)
+
+
+;; Coffee-mode
+(add-to-list 'load-path "~/.emacs.d/coffee-mode")
+(require 'coffee-mode)
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+
+  ;; CoffeeScript uses two spaces.
+  (set (make-local-variable 'tab-width) 2)
+
+  ;; If you don't want your compiled files to be wrapped
+  (setq coffee-args-compile '("-c" "--no-wrap"))
+
+  ;; *Messages* spam
+  (setq coffee-debug-mode t))
+
+
+(add-hook 'coffee-mode-hook '(lambda () (coffee-custom)))
