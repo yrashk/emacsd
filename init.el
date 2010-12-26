a143bcc3fe8299c2f4c6715330865bf4e79badb7
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-26 13:06:22 -0800

    Added support for Erlang R14B01

--- init.el
+++ init.el
@@ -44,6 +44,7 @@
 
 ;; Erlang
 (setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.6.1/emacs"  load-path))
+(setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.6.2/emacs" load-path))
 (setq erlang-root-dir "/usr/local/lib/erlang")
 (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
 (require 'erlang-start)
