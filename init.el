68f49fa0f418304847770b857118253af0084328
Yurii Rashkovskii <yrashk@gmail.com>
2012-03-23 18:34:49 -0700

    Adds support for erlang from ubuntu 11

--- init.el
+++ init.el
@@ -49,6 +49,7 @@
 (setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.6.1/emacs"  load-path))
 (setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.6.2/emacs" load-path))
 (setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.6.3/emacs" load-path))
+(setq load-path (cons  "/usr/lib/erlang/lib/tools-2.6.6.3/emacs" load-path))
 (setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.6.4/emacs" load-path))
 (setq erlang-root-dir "/usr/local/lib/erlang")
 (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
